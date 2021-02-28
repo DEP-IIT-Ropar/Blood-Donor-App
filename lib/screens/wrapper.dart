dfe0e41f0bd5a44256152eaa87f8c5707c75ed35
vamsi-cham <62752615+vamsi-cham@users.noreply.github.com>
2021-02-28 20:11:05 +0530

    Add files via upload

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/models/user.dart';
+import 'package:myapp/screens/authenticate/authenticate.dart';
+
+
+import 'package:myapp/screens/home/home.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+   final user = Provider.of<User>(context);
+
+   // return home or authenticate widget
+
+    if(user == null){
+      return Authenticate();
+    } else{
+      return Home();
+    }
+  }
+}
